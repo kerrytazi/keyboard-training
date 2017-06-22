313f0dab2e2f67e0fc290603692f8ba44046572e
kerrytazi <kerrytazi@mail.ru>
2017-06-23 04:33:55 +0900

    START

---
+++ index.go
@@ -0,0 +1,97 @@
+package main
+
+
+import (
+    "log"
+    "os"
+    "strconv"
+    "time"
+    "math/rand"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "path/filepath"
+)
+
+
+var CURRENT string;
+var FILES = make(map[string]string);
+var TEXTS = make(map[string]int);
+var PORT = os.Getenv("PORT");
+
+
+func init() {
+    cur, err := filepath.Abs(".");
+    if err != nil {
+        log.Fatal(err);
+    }
+
+    if len(PORT) == 0 {
+        PORT = "80";
+    }
+
+    CURRENT = cur;
+    FILES["/"] = filepath.Join(CURRENT, "public",  "index.html");
+    FILES["/main.css"] = filepath.Join(CURRENT, "public", "main.css");
+    FILES["/main.js"] = filepath.Join(CURRENT, "public", "main.js");
+    FILES["/vue.js"] = filepath.Join(CURRENT, "public", "vue.js");
+    FILES["/vue.min.js"] = filepath.Join(CURRENT, "public", "vue.min.js");
+
+    locales, _ := ioutil.ReadDir(filepath.Join(CURRENT, "texts"));
+    for _, locale := range locales {
+        name := locale.Name();
+        t, _ := ioutil.ReadDir(filepath.Join(CURRENT, "texts", name));
+        TEXTS[name] = len(t);
+    }
+
+    rand.Seed(time.Now().UTC().UnixNano());
+}
+
+
+func getLocale(rawQuery string) string {
+    urlQueryMap, err := url.ParseQuery(rawQuery);
+    if err != nil {
+        return "en";
+    }
+
+    if val, ok := urlQueryMap["locale"]; ok {
+        return val[0];
+    }
+
+    return "en";
+}
+
+
+func getRandomTextFile(locale string) string {
+    textsLen := TEXTS[locale];
+    ind := strconv.Itoa(rand.Intn(textsLen));
+    return "/texts/" + locale + "/" + ind;
+}
+
+
+func handleRedirect(res http.ResponseWriter, req *http.Request) {
+    link := getRandomTextFile(getLocale(req.URL.RawQuery));
+    http.Redirect(res, req, link, 302);
+}
+
+
+func handleRawPath(res http.ResponseWriter, req *http.Request) {
+    path := filepath.Join(CURRENT, req.URL.Path);
+    http.ServeFile(res, req, path);
+}
+
+
+func handleSpecialPath(res http.ResponseWriter, req *http.Request) {
+    path := req.URL.Path;
+    if _, ok := FILES[path]; !ok { return; }
+    http.ServeFile(res, req, FILES[path]);
+}
+
+
+func main() {
+    http.HandleFunc("/", handleSpecialPath);
+    http.HandleFunc("/texts/", handleRawPath);
+    http.HandleFunc("/locales/", handleRawPath);
+    http.HandleFunc("/random_text/", handleRedirect);
+    log.Fatal(http.ListenAndServe(":" + PORT, nil));
+}
---
+++ public/main.js
@@ -0,0 +1,213 @@
+(() => {"use strict";
+
+
+
+const STATS = {
+    time: 0,
+    speed: 0,
+    av: 0,
+    acc: 100,
+    max: 0,
+    total: 0,
+    nErrors: 0
+}
+
+
+
+
+window.app = new Vue({
+    el: '#vue_container',
+
+    data: {
+
+        stats: Object.assign({}, STATS),
+        localeFace: {},
+
+        settings: {
+            stamina: false,
+
+            timer: {
+                enabled: false,
+                time: 60,
+                func: 0
+            },
+
+            randomText: true,
+
+            locale: {
+                faceLang: localStorage.face || 'en',
+                textsLang: localStorage.texts || 'en',
+                list: ['en', 'ru']
+            }
+        },
+
+        textSource: '',
+        text: '',
+        distance: 17,
+        pos: 0,
+        errorFull: '',
+        taps: [],
+
+        stage: 0,
+        startTime: 0,
+        updateInterval: 0,
+
+        settingsMode: false,
+        editMode: false
+    },
+
+    methods: {
+        onKeyDown(event) {
+            if ((this.stage === 2) ||
+                (event.key.length > 1 && event.keyCode !== 8) ||
+                (!this.text) ||
+                (this.editMode) ||
+                (this.settingsMode)
+            ) return;
+
+            if (this.stage === 0) this.start();
+
+            event.preventDefault();
+
+
+            if (event.keyCode === 8) {
+                if (!this.settings.stamina) return this.erease();
+                return;
+            }
+
+            if (event.key !== this.text[this.pos] || this.errorFull) {
+                this.stats.nErrors++;
+                if (!this.settings.stamina) this.errorFull += event.key;
+            } else {
+                this.taps.push(performance.now());
+                this.pos++;
+                if (this.pos === this.text.length) this.stop();
+            }
+
+            this.stats.total++;
+            this.updateStats();
+        },
+
+        erease() {
+            if (!this.errorFull) return (this.pos) ? this.pos-- : null;
+            this.errorFull = this.errorFull.slice(0, -1);
+        },
+
+        start() {
+            this.startTime = performance.now();
+            this.updateInterval = setInterval(this.updateStats, 1000);
+            this.stage = 1;
+            
+            let { timer } = this.settings;
+            if (timer.enabled) {
+                timer.func = setTimeout(this.stop, timer.time * 1000);
+            }
+        },
+
+        updateStats() {
+            let now = performance.now();
+            let { stats, startTime, updateTaps } = this;
+            
+            updateTaps(now);
+
+            let valid = stats.total - stats.nErrors;
+            let av = Math.floor(valid / stats.time * 60);
+            let acc = 100 / (stats.total / (stats.total - stats.nErrors));
+
+            stats.speed = this.taps.length;
+            stats.time = Math.round((now - startTime) / 1000);
+            stats.av = (av < 2000) ? av : 2000;
+            stats.acc = isFinite(acc) ? Math.floor(acc) : 0;
+            if (stats.max < stats.speed) stats.max = stats.speed;
+        },
+
+        updateTaps(now) {
+            let ind = this.taps.findIndex(t =>  t + 60000 > now);
+            if (ind) this.taps = (ind > -1) ? this.taps.slice(ind) : [];
+        },
+
+        stop() {
+            if (!this.stage) return;
+            clearInterval(this.updateInterval);
+            clearTimeout(this.settings.timer.func);
+            this.stage = 2;
+            this.updateStats();
+        },
+
+        reset() {
+            if (!this.stage) return;
+            clearInterval(this.updateInterval);
+            clearTimeout(this.settings.timer.func);
+            this.stage = this.pos = 0;
+            this.errorFull = '';
+            this.taps = [];
+            Object.assign(this.stats, STATS);
+        },
+
+        randomize(text) {
+            return text
+                .match(/[^\s].+?[.!?\s]+(?=\s)/g)
+                .sort(() => Math.random() - .5)
+                .join(' ')
+                .replace(/\n\s+/g, '\n');
+        },
+
+        async newText() {
+            let locale = this.settings.locale.textsLang;
+            let res = await fetch(`/random_text/?locale=${locale}`);
+            let text = await res.text();
+            let isRand = this.settings.randomText;
+            this.textSource = isRand ? this.randomize(text) : text;
+        },
+
+        async newLocale() {
+            let locale = this.settings.locale.faceLang;
+            let res = await fetch(`/locales/${locale}.json`);
+            this.localeFace = await res.json();
+        }
+    },
+
+    computed: {
+        pass() {
+            let { text, pos, distance, errorFull } = this;
+            let from = pos - distance + errorFull.length;
+            let chars = (from < pos) ? text.substring(from, pos) : '';
+            let nSpaces = distance - chars.length - errorFull.length;
+            return ' '.repeat((nSpaces > 0) ? nSpaces : 0) + chars;
+        },
+
+        error() {
+            let from = this.errorFull.length - this.distance;
+            return this.errorFull.substring(from);
+        }
+    },
+
+    watch: {
+        textSource(t) {
+            this.reset();
+            this.text = t
+                .replace(/\t+|\n+|\s{2,}/g, ' ')
+                .replace(/«|»/g, '"')
+                .replace(/—/g, '-');
+        },
+
+        'settings.locale.faceLang': function(val) {
+            localStorage.face = val;
+            this.newLocale();
+        },
+
+        'settings.locale.textsLang': function(val) {
+            localStorage.texts = val;
+            this.newText();
+        }
+    },
+
+    mounted() {
+        this.newLocale();
+        this.newText();
+        window.addEventListener('keydown', this.onKeyDown);
+    }
+});
+
+
+})();
